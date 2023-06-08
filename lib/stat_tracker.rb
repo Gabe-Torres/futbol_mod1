9761d7ccaf20d0d31f18a1d135f04e3221bf8662
Davis Weimer <davisweimer@gmail.com>
2023-06-07 18:54:38 -0600

    fix: Add initialized attribute arrays for the CSV objects

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,41 +1,43 @@
 require "csv"
 
 class StatTracker
-  
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize
+    @games = []
+    @teams = []
+    @game_teams =[]
+  end
+
   def self.from_csv(locations)
-    game_hash = {}
     stat_tracker = StatTracker.new
     
-    game_hash[:games] = stat_tracker.game_generator(locations)
-    game_hash[:teams] = stat_tracker.teams_generator(locations)
-    game_hash[:game_teams] = stat_tracker.game_teams_generator(locations)
-    game_hash
+    stat_tracker.game_generator(locations)
+    stat_tracker.teams_generator(locations)
+    stat_tracker.game_teams_generator(locations)
+    return stat_tracker
   end
   
   # helper methods
   
   def game_generator(locations)
-    games =[]
     CSV.foreach locations[:games], headers: true, header_converters: :symbol do |row|
-      games << Game.new(row[:game_id], row[:season], row[:type], row[:date_time], row[:away_team_id], row[:home_team_id], row[:away_goals], row[:home_goals], row[:venue])
+      @games << Game.new(row[:game_id], row[:season], row[:type], row[:date_time], row[:away_team_id], row[:home_team_id], row[:away_goals], row[:home_goals], row[:venue])
     end
-    games
   end
   
   def teams_generator(locations)
-    teams = []
     CSV.foreach locations[:teams], headers: true, header_converters: :symbol do |row|
-      teams << Team.new(row[:team_id], row[:franchiseid], row[:teamname], row[:abbreviation], row[:stadium])
+      @teams << Team.new(row[:team_id], row[:franchiseid], row[:teamname], row[:abbreviation], row[:stadium])
     end
-    teams
   end
   
   def game_teams_generator(locations)
-    game_teams =[]
     CSV.foreach locations[:game_teams], headers: true, header_converters: :symbol do |row|
-      game_teams << GameTeam.new(row[:game_id], row[:team_id], row[:hoa], row[:result], row[:settled_in], row[:head_coach], row[:goals], row[:shots], row[:tackles], row[:pim], row[:powerplayopportunities], row[:powerplaygoals], row[:faceoffwinpercentage], row[:giveaways], row[:takeaways])
+      @game_teams << GameTeam.new(row[:game_id], row[:team_id], row[:hoa], row[:result], row[:settled_in], row[:head_coach], row[:goals], row[:shots], row[:tackles], row[:pim], row[:powerplayopportunities], row[:powerplaygoals], row[:faceoffwinpercentage], row[:giveaways], row[:takeaways])
     end
-    game_teams
   end
   
 end
